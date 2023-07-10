20b27c117737340a8941d9bdf38ab866465565ee
Sunho <sunh803@naver.com>
2023-07-10 22:15:22 +0900

    chore: gitignore추가xcode세팅

---
+++ LoveCrane/LoveCrane/LoveCraneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveCraneApp.swift
+//  LoveCrane
+//
+//  Created by 235 on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct LoveCraneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
